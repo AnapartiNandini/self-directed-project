29bc9938cc13d1b6909be02f22d1c42cd2ecbb53
Nandini Anaparti <nandini.anaparti@gmail.com>
2021-06-04 14:32:59 -0500

    html and css for navbar complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
+import{Link} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <>
+    <header>
+      <ul lassName="leftSide">
+        <li><Link className="link" to="/">Home</Link></li>
+        <li><Link className="link" to="/shopping">shopping</Link></li>
+        <li><Link className="link" to="/add-post">Post</Link></li>
+      </ul>
+      <ul lassName="rightSide">
+        <li><Link className="link" to="/search">search</Link></li>
+        <li><Link className="link" to="/notifications">notifications</Link></li>
+        <li><Link className="link" to="/profile">Profile</Link></li>
+      </ul>
+    </header>
     </>
   );
 }
