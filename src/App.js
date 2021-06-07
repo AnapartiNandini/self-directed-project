3d57b97899a67f879854ea15dcdd6120e9b18c32
Nandini Anaparti <nandini.anaparti@gmail.com>
2021-06-07 13:03:52 -0500

    nav items vertically centered

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import{Link} from "react-router-dom";
-import './App.css';
-import logo from  "./MITTPosts-Logo.PNG";
+import { Link } from "react-router-dom";
+import "./App.css";
+import logo from "./MITTPosts-Logo.PNG";
 
 function App() {
   return (
     <>
-    <header>
-     <div>
-      <ul>
-        <li className="leftSide"><Link className="link" id="logo" to="/"><img src={logo} alt="MITTPosts logo"/></Link></li>
-        <li className="leftSide"><Link className="link" to="/shopping">Shopping</Link></li>
-        <li className="leftSide"><Link className="link" to="/add-post">Post</Link></li>
-        <li className="rightSide"><Link className="link" to="/profile">Profile</Link></li>
-        <li className="rightSide"><Link className="link" to="/notifications">Notifications</Link></li>
-        <li className="rightSide"><Link className="link" to="/search">Search</Link></li>
-      </ul>
-      </div>
-    </header>
+      <header>
+        <ul>
+          <li className="leftSide">
+            <Link className="link" id="logo" to="/">
+              <img src={logo} alt="MITTPosts logo" />
+            </Link>
+          </li>
+          <li className="leftSide">
+            <Link className="link" to="/shopping">
+              Shopping
+            </Link>
+          </li>
+          <li className="leftSide">
+            <Link className="link" to="/add-post">
+              Post
+            </Link>
+          </li>
+          <li className="rightSide">
+            <Link className="link" to="/profile">
+              Profile
+            </Link>
+          </li>
+          <li className="rightSide">
+            <Link className="link" to="/notifications">
+              Notifications
+            </Link>
+          </li>
+          <li className="rightSide">
+            <Link className="link" to="/search">
+              Search
+            </Link>
+          </li>
+        </ul>
+      </header>
     </>
   );
 }
