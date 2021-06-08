0f3b0c6c190d904aba050f3fc4aadea5bf184e06
Nandini Anaparti <nandini.anaparti@gmail.com>
2021-06-08 12:28:25 -0500

    profile image added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import "./App.css";
 import logo from "./MITTPosts-Logo.PNG";
+import profileImage from "./profile-image.PNG";
 
 function App() {
   return (
@@ -59,42 +60,29 @@ function App() {
         <div className="tracking-in-expand-fwd">
           <div className="sidebar-profile">
             {/* sample profile image */}
-            <img src="img/pfpexample.jpg" alt="Profile Photo" />
+            <img src={profileImage} alt="Profile Photo" />
             <h3 className="user-name"></h3>
             <p className="user-email"></p>
 
             <div className="sidebar-options">
               <ul>
                 <li>
-                  <Link to="">
+                  <Link to="" className="sidebar-link">
                     <span class="material-icons-outlined">groups</span>Followers
                   </Link>
                 </li>
                 <li>
-                  <Link to="">
+                  <Link to="" className="sidebar-link">
                     <span class="material-icons-outlined">groups</span>Following
-                    My Music
                   </Link>
                 </li>
                 <li>
-                  <Link to="">
+                  <Link to="" className="sidebar-link">
                     <span class="material-icons-outlined">settings</span>Settings
                   </Link>
                 </li>
                 <li>
-                  <Link to="">
-                    <span className="material-icons-outlined">podcasts</span>
-                    Podcast
-                  </Link>
-                </li>
-                <li>
-                  <Link to="">
-                    <span className="material-icons-outlined">music_video</span>
-                    Artists
-                  </Link>
-                </li>
-                <li>
-                  <Link to="">
+                  <Link to=""className="sidebar-link">
                     <span className="material-icons-outlined">logout</span>
                     Logout
                   </Link>
