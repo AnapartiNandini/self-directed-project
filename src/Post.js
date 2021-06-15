2bf84c3a908e787e5217243d03e03462a387f2eb
Nandini Anaparti <nandini.anaparti@gmail.com>
2021-06-15 14:51:39 -0500

    comment button added to each post

--- src/Post.js
+++ src/Post.js
@@ -1,23 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-class Post extends React.Component {
-  render() {
-    let image;
-    if(this.props.image == "") {
-      image = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/480px-No_image_available.svg.png"
-    } else {
-      image = this.props.image;
-    }
-    return (
-      <li className="post">
-        <img src={image} alt="" />
-        <div className="post-info">
-          <div className="post-title">{this.props.title}</div>
-          <div className="description">{this.props.description}</div>
-        </div>
-      </li>
-    );
+const Post = () => {
+  let image;
+  if (this.props.image == "") {
+    image =
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/480px-No_image_available.svg.png";
+  } else {
+    image = this.props.image;
   }
-}
+  return (
+    <li className="post">
+      <img src={image} alt="" />
+      <div className="post-info">
+        <div className="post-title">{this.props.title}</div>
+        <div className="description">{this.props.description}</div>
+        <Link to="/comment-page">
+          <button className="comment-btn">Comment</button>
+        </Link>
+      </div>
+    </li>
+  );
+};
 
 export default Post;
